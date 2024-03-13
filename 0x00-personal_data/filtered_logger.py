d136b9cbef84a58c264aae0a043b5345eec8dfc9
Ebuube <onwutaebubegideon1555@gmail.com>
2024-03-13 11:53:14 +0000

    Anotate function

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,15 +4,16 @@
 import re
 from typing import List
 
-def filter_datum(fields: List[str] = [], rep: str = 'xxx', msg: str ='',
-        sep: str =';') -> str:
+
+def filter_datum(fields: List[str] = [], redaction: str = 'xxx',
+                 message: str = '', separator: str = ';') -> str:
     """Obfuscate sensitive fields in a log line
-    rep: replacment string
-    msg: message to obfuscate
-    sep: separators to use
+    redaction: redactionlacment string
+    message: message to obfuscate
+    separator: separators to use
     Pattern -> '{field}={val}(optional semicolon)'
     """
     for field in fields:
         pattern = '(?P<field>{})=(?P<val>.*?)(?:;|$)'.format(field)
-        msg = re.sub(pattern, lambda m: f"{m.group('field')}={rep}{sep}", msg)
-    return msg
+        message = re.sub(pattern, lambda m: f"{m.group('field')}={redaction}{separator}", message)
+    return message
