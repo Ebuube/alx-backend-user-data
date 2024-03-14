cc290a45bf1a7b887e32a1ffaaf7ed77f8bb1f22
Ebuube <onwutaebubegideon1555@gmail.com>
2024-03-14 11:06:12 +0000

    Modified format

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -33,16 +33,15 @@ class RedactingFormatter(logging.Formatter):
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name) %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
-    FIELDS = []
 
     def __init__(self, fields: List[str] = []):
         super(RedactingFormatter, self).__init__(self.FORMAT)
-        self.FIELDS = fields
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         """Return the customized format result
         """
-        msg = super().format(record)   # get original message
-        fmt_msg = filter_datum(fields=self.FIELDS, redaction=self.REDACTION,
+        msg = super().format(record)
+        fmt_msg = filter_datum(fields=self.fields, redaction=self.REDACTION,
                                message=msg, separator=self.SEPARATOR)
         return fmt_msg
