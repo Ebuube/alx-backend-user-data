f992a18528f505e4266271466845e2db539ebff1
Ebuube <onwutaebubegideon1555@gmail.com>
2024-03-17 11:53:57 +0000

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,10 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized', strict_slashes=False)
+def unauthorized() -> str:
+    """Test that error 401 is obtained
+    """
+    abort(401)
