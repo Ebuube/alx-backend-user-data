40708271b15f1565b461514170440235bc729b2e
Ebuube <onwutaebubegideon1555@gmail.com>
2024-03-17 14:36:16 +0000

    Task 13: Require auth with stars

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -19,7 +19,13 @@ class Auth:
             path = path + '/'
         if path not in excluded_paths:
             return True
-        return False
+        for excluded in excluded_paths:
+            if excluded[-1] == '*':
+                if path.startswith(excluded[0:-1]):
+                    return False
+            elif path == excluded:
+                return False
+        return True
 
     def authorization_header(self, request=None) -> str:
         """Handler for authorization header
