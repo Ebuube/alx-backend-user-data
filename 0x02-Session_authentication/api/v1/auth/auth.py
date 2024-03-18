942b8732293618c9400f128b7546e98d6b6961fe
Ebuube <onwutaebubegideon1555@gmail.com>
2024-03-18 19:54:30 +0000

    Task 4: Session cookie

--- 0x02-Session_authentication/api/v1/auth/auth.py
+++ 0x02-Session_authentication/api/v1/auth/auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Handle authentication
 """
+import os
 from flask import request
 from typing import List, TypeVar
 
@@ -38,3 +39,10 @@ class Auth:
         """Check the current user
         """
         return None
+
+    def session_cookie(self, request=None):
+        """Return the value of the session_id from the cookie
+        """
+        if request is None:
+            return None
+        return request.cookies.get(os.getenv('SESSION_NAME'))
