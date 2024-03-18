ca18afbae983cc53719d33dd9cac27b1598e0c1a
Ebuube <onwutaebubegideon1555@gmail.com>
2024-03-18 19:35:58 +0000

    Task 2: Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,9 +2,19 @@
 """Session authentication
 """
 from api.v1.auth.auth import Auth
+from uuid import uuid4
 
 
 class SessionAuth(Auth):
     """Authenticate clients using Session Authentication
     """
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """Generate a session id for a user
+        """
+        if user_id is None or type(user_id) is not str:
+            return None
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
