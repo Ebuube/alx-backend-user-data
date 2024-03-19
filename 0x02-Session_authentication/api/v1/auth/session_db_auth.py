ad975b9b7a9c7bbaa6c96639c8d3f436196d1767
Ebuube <onwutaebubegideon1555@gmail.com>
2024-03-19 00:11:44 +0000

    Task 10: Sessions in database

--- 0x02-Session_authentication/api/v1/auth/session_db_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_db_auth.py
@@ -2,6 +2,7 @@
 """Persisted Authentication
 """
 from api.v1.auth.session_exp_auth import SessionExpAuth
+from datetime import datetime, timedelta
 from models.user_session import UserSession
 
 
@@ -11,6 +12,8 @@ class SessionDBAuth(SessionExpAuth):
     def create_session(self, user_id=None):
         """Create and persist a User session
         """
+        if not user_id:
+            return None
         session_id = super().create_session(user_id)
         user_session = UserSession(user_id=user_id, session_id=session_id)
         user_session.save()
@@ -19,13 +22,21 @@ class SessionDBAuth(SessionExpAuth):
     def user_id_for_session_id(self, session_id=None):
         """Get the user_id using a session
         """
+        if session_id is None:
+            return None
         matches = UserSession.search(attributes={'session_id': session_id})
         if not matches or len(matches) == 0:
             return None
         user_session = matches[0]
 
+        if self.session_duration <= 0:
+            return user_session.user_id
         # Check if session has expired by adding the 'created_at' attribute
         # to UserSession instance
+        max_datetime = user_session.created_at + \
+                timedelta(seconds=self.session_duration)    # noqa: E502, E128
+        if max_datetime < datetime.now():
+            return None
         return user_session.user_id
 
     def destroy_session(self, request=None):
