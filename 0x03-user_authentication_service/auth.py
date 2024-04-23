454a43adea673f74b82cdde6e6880353ce47baa7
Ebuube <onwutaebubegideon1555@gmail.com>
2024-04-23 18:20:34 +0000

    Task 8: Credentials validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -33,3 +33,12 @@ class Auth:
         hashed_pwd = _hash_password(password)
         user = self._db.add_user(email=email, hashed_password=hashed_pwd)
         return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """Validate credentials
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+        except NoResultFound:
+            return False
+        return bcrypt.checkpw(password.encode('utf-8'), user.hashed_password)
