b2e6a24f843f1d3b89a63fc898d957e8d8db1bd6
Ebuube <onwutaebubegideon1555@gmail.com>
2024-04-23 17:39:18 +0000

    Task 7: Registered user

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 """Minimal Flask app
 """
-from flask import Flask, jsonify
+from auth import Auth
+from flask import Flask, jsonify, request
 
 
+AUTH = Auth()
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
@@ -13,6 +15,31 @@ def home():
     """Home route
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", methods=["POST"])
+def register_users():
+    """Register a new user
+    Parameters in JSON payload:
+        * email: str
+        * password: str
+    """
+    try:
+        rj = request.get_json()
+    except Exception as e:
+        print(f"Error in getting json: {e}")    # test
+        return jsonify({"Error": "JSON payload missing"}), 400
+    required = ['email', 'password']
+    for attr in required:
+        if attr not in rj:
+            return jsonify({"Error": f"{attr} missing"}), 400
+    try:
+        user = AUTH.register_user(rj['email'], rj['password'])
+    except ValueError as e:
+        print(e)
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({
+        "email": user.email, "message": "user created"})
 
 
 if __name__ == "__main__":
