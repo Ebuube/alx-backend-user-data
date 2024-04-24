78cfb50175d071ec1997c804698381b4cf1a8cbf
Ebuube <onwutaebubegideon1555@gmail.com>
2024-04-24 15:04:08 +0000

    Renamed method

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -61,7 +61,7 @@ class Auth:
         self._db.update_user(user_id=user.id, session_id=session_id)
         return session_id
 
-    def get_use_from_session_id(self, session_id: str) -> str:
+    def get_user_from_session_id(self, session_id: str) -> str:
         """Return user bearing this session id
         """
         if not session_id:
