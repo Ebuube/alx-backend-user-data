f889ace4f4aa9d276c06ee9852fc769a6ac6b1ca
Ebuube <onwutaebubegideon1555@gmail.com>
2024-04-24 16:47:20 +0000

    Task 19: Update password end-point

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -101,5 +101,28 @@ def get_reset_password_token():
     return jsonify({"email": f"{email}", "reset_token": reset_token}), 200
 
 
+@app.route("/reset_password", methods=["PUT"])
+def update_password():
+    """Update as user password
+    Form-data parameters:
+        * email: str
+        * reset_token: str
+        * new_password: str
+    """
+    required = ['email', 'reset_token', 'new_password']
+    for attr in required:
+        if attr not in request.form:
+            return jsonify({"Error": f"{attr} missing"}), 403
+
+    email = request.form.get('email')
+    reset_token = request.form.get('reset_token')
+    password = request.form.get('new_password')
+    try:
+        AUTH.update_password(reset_token, password)
+    except ValueError:
+        abort(403, 'Invalid reset token')
+    return jsonify({"email": email, "message": "Password updated"}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000", debug=True)
