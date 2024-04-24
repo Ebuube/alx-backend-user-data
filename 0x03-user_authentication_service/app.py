7e619930d6767443e23a8bbcdc09e1c69b068ac0
Ebuube <onwutaebubegideon1555@gmail.com>
2024-04-24 14:56:47 +0000

    Task Log in

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """Minimal Flask app
 """
 from auth import Auth
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, make_response
 
 
 AUTH = Auth()
@@ -20,7 +20,7 @@ def home():
 @app.route("/users", methods=["POST"])
 def register_users():
     """Register a new user
-    Parameters in JSON payload:
+    Parameters in Form data:
         * email: str
         * password: str
     """
@@ -37,6 +37,32 @@ def register_users():
         return jsonify({"message": "email already registered"}), 400
     return jsonify({
         "email": user.email, "message": "user created"})
+
+
+@app.route("/sessions", methods=["POST"])
+def login():
+    """Create a session for user
+    Parameters in Form data:
+        * email: str
+        * password: str
+    """
+    required = ['email', 'password']
+    for attr in required:
+        if attr not in request.form:
+            print(f"{attr} is missing")     # test
+            abort(401)
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not AUTH.valid_login(email, password):
+        print("Invalid login credentials")  # test
+        abort(401)
+
+    response = make_response(jsonify({"email": email, "message": "logged in"}))
+    response.set_cookie("session_id", AUTH.create_session(email))
+    return response
+
+    return ''
 
 
 if __name__ == "__main__":
